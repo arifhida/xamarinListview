a568c47049d0453e9148f665a25753422cefc5d6
areefhida <areef.hiide@gmail.com>
2017-09-25 08:05:11 +0700

    Add project files.

---
+++ XamarinListView/MainActivity.cs
@@ -0,0 +1,71 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using XamarinListView.model;
+
+namespace XamarinListView
+{
+    [Activity(Label = "XamarinListView", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private List<Person> mList;
+        private ListView mListview;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            mListview = FindViewById<ListView>(Resource.Id.myListview);
+            mList = createList();
+            myListAdapter adapter = new myListAdapter(this, mList);
+            mListview.Adapter = adapter;
+            mListview.ItemClick += MListview_ItemClick;
+        }
+
+        private void MListview_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private List<Person> createList()
+        {
+            var list = new List<Person>();
+
+            list.Add(new Person()
+            {
+                FirstName = "John",
+                LastName = "McClane",
+                Age = "22",
+                Gender = "Male"
+            });
+            list.Add(new Person()
+            {
+                FirstName = "Jerry",
+                LastName = "McGuire",
+                Age = "35",
+                Gender = "Male"
+            });
+            list.Add(new Person()
+            {
+                FirstName = "Marry",
+                LastName = "Curry",
+                Age = "50",
+                Gender = "Female"
+            });
+
+            list.Add(new Person()
+            {
+                FirstName = "Peter",
+                LastName = "Parker",
+                Age = "20",
+                Gender = "Male"
+            });
+
+            return list;
+        }
+    }
+}
+
---
+++ XamarinListView/myListAdapter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamarinListView.model;
+
+namespace XamarinListView
+{
+    class myListAdapter : BaseAdapter<Person>
+    {
+        private List<Person> mItems;
+        private Context mcontext;
+
+        public myListAdapter(Context context,List<Person> items)
+        {
+            mItems = items;
+            mcontext = context;
+        }
+        public override Person this[int position]
+        {
+            get
+            {
+                return mItems[position];
+            }
+        }
+
+        public override int Count
+        {
+            get { return mItems.Count; }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View row = convertView;
+            if(row == null)
+            {
+                row = LayoutInflater.From(mcontext).Inflate(Resource.Layout.listViewRow, null, false);
+            }
+            TextView txtName = row.FindViewById<TextView>(Resource.Id.txtName);
+            txtName.Text = mItems[position].FirstName;
+            TextView txtLastName = row.FindViewById<TextView>(Resource.Id.txtLastName);
+            txtLastName.Text = mItems[position].LastName;
+            TextView txtAge = row.FindViewById<TextView>(Resource.Id.txtAge);
+            txtAge.Text = mItems[position].Age;
+            TextView txtGender = row.FindViewById<TextView>(Resource.Id.txtGender);
+            txtGender.Text = mItems[position].Gender;
+            return row;
+        }
+    }
+}
